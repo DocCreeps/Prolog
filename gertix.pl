c09e349f19fa9037f07fd699b03bd6911e19dc36
Dorian <33058222+DocCreeps@users.noreply.github.com>
2021-05-10 20:07:15 +0200

    Update gertix.pl

--- gertix.pl
+++ gertix.pl
@@ -1,41 +1,50 @@
 :-dynamic compteur/2.
-†
-% compteur(Joue, Gagne).
+ 
+% fonction compteur(Joue, Gagne)
 compteur(0,0).
-†
-†
+ 
+% fonction lancement de la partie si gagant
 lancement(gagne, Mise, Pari, Resultat) :-
-††††Resultat is Mise + Pari.
-†
+    Resultat is Mise + Pari.
+% fonction lancement de la partie si perdant
 lancement(perdu, Mise, Pari, Resultat) :-
-††††Resultat is Mise - Pari.
-†
+    Resultat is Mise - Pari.
+ % fonction qui g√©n√©re le resultat
 gen_resultat(Resultat) :-
-††††N is random(2),
-††††(†† N = 0
-††††->† Resultat = perdu
-††††;†† Resultat = gagne).
-††
-†
-†
-†
+    N is random(2),
+    (   N = 0
+    ->  Resultat = perdu
+    ;   Resultat = gagne).
+  
+ 
+ 
+% fonction qui execute le d√©roulement de la partie
 jeu(Mise_init,Strategie, Valeur):-
-††††(†† Mise_init =< 0
-††††->† writeln('You loose !'),
-††††††††compteur(Joue, Gagne),
-††††††††format('Parties Jouees ~w, gagnees ~w~n', [Joue, Gagne])
-††††;†† % calcul du montant du pari
-††††††††writeln('Avoir ': Mise_init),
-††††††††(†† Strategie = 1
-††††††††->† Pari is  round(Mise_init * Valeur / 100)
-††††††††;††Strategie = 2 -> Pari = Valeur),
-††††††††††††% obtention du resultat du pari (gagne/perdu)
-††††††††gen_resultat(Resultat),
-††††††††retract(compteur(J, G)),
-††††††††J1 is J+1,
-††††††††(†† Resultat = gagne
-††††††††->†† G1 is G+1
-††††††††;†† G1 = G),
-††††††††assert(compteur(J1, G1)),
-††††††††lancement(Resultat, Mise_init, Pari, Nouvelle_Mise),
-††††††††jeu(Nouvelle_Mise, Strategie, Valeur)).
+    % on verifie que la mise est superieur a 0
+    (   Mise_init =< 0
+     %on ecrit Vous √™tes runin√©.
+    ->  writeln('Vous √™tes ruin√©!'),
+     %on incr√©mete le compteur si il gagne
+        compteur(Joue, Gagne),
+     %on calcul le nombre de partie perdu
+     Perdu is Joue - Gagne,
+     %on affiche le nombre de partie total,gagner et perdu
+        format('Parties Jouees ~w, gagnees ~w~n, Perdu ~w~n ', [Joue, Gagne,Perdu])
+    ;   % calcul du montant du pari
+        writeln('Avoir ': Mise_init),
+     %verification de la strategie utilis√© si 1 en % si 2 la mise = le nombre definie
+        (   Strategie = 1
+        ->  Pari is  round(Mise_init * Valeur / 100)
+        ;  Strategie = 2 -> Pari = Valeur),
+            % obtention du resultat du pari (gagne/perdu)
+        gen_resultat(Resultat),
+     %recursivit√©.
+        retract(compteur(J, G)),
+     %si resultat= gagne incremente le compteur gagne
+        J1 is J+1,
+        (   Resultat = gagne
+        ->   G1 is G+1
+        ;   G1 = G),
+        assert(compteur(J1, G1)),
+        lancement(Resultat, Mise_init, Pari, Nouvelle_Mise),
+        jeu(Nouvelle_Mise, Strategie, Valeur)).
